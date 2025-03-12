1307eba38b41e1d34a82936447d2fd03696926f0
Daniil Bylkou <daniil.bylkou@innowise.com>
2025-03-12 13:39:35 +0300

    test implemented

--- src/test/java/by/kovalski/jwtauth/JwtAuthApplicationTests.java
+++ src/test/java/by/kovalski/jwtauth/JwtAuthApplicationTests.java
@@ -1,5 +1,6 @@
 package by.kovalski.jwtauth;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class JwtAuthApplicationTests {
 
     @Test
     void contextLoads() {
+        Assertions.assertTrue(true);
     }
 
 }
