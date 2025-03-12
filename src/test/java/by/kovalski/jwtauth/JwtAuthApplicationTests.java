24511e1308f8dd19cd0d2dea998924c1d3b674e7
Daniil Bylkou <daniil.bylkou@innowise.com>
2025-03-12 14:11:04 +0300

    test fix

--- src/test/java/by/kovalski/jwtauth/JwtAuthApplicationTests.java
+++ src/test/java/by/kovalski/jwtauth/JwtAuthApplicationTests.java
@@ -1,11 +1,24 @@
 package by.kovalski.jwtauth;
 
+import by.kovalski.jwtauth.repository.UserDataRepository;
+import by.kovalski.jwtauth.repository.UserRepository;
+import jakarta.persistence.EntityManagerFactory;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class JwtAuthApplicationTests {
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @MockBean
+    private UserDataRepository userDataRepository;
+
+    @MockBean
+    private EntityManagerFactory entityManagerFactory;
 
     @Test
     void contextLoads() {
